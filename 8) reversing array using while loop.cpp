7c3015ac98ef69e16f6621d1971248297faffebd
Tofik Khan <114330109+XDigital123@users.noreply.github.com>
2024-02-12 18:48:12 +0530

    Add files via upload

---
+++ 8) reversing array using while loop.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size) {
+int left = 0; 
+int right = size-1; 
+
+//reversing array using while loop 
+while(left<=right) {
+   swap(arr[left], arr[right]);
+   left++;
+   right--;
+}
+  
+
+   //printing 
+   for(int i=0; i<size; i++) {
+      cout<<arr[i]<<"  ";
+   }
+}
+
+int main() {
+
+   int arr[] = {10,20,30,40,50,60};
+   int size = sizeof(arr)/ sizeof(arr[0]);
+  reverseArray(arr,size);
+   return 0;
+}
